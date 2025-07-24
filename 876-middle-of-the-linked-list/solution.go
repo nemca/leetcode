7e818c4a12a5081b5990236dfc42acf4cdcbcc67
Michael Bruskov <mixanemca@yandex.ru>
2025-07-24 16:38:43 +0400

    add 876-middle-of-the-linked-list

---
+++ 876-middle-of-the-linked-list/solution.go
@@ -0,0 +1,31 @@
+package middle_of_the_linked_list
+
+import (
+	"github.com/nemca/leetcode/models"
+)
+
+func middleNode(head *models.ListNode) *models.ListNode {
+	if head.Next == nil {
+		return head
+	}
+
+	count := 1
+	next := head
+	ll := make(map[int]*models.ListNode)
+
+	for next.Next != nil {
+		count++
+		next = next.Next
+		ll[count] = next
+	}
+
+	if count%2 != 0 {
+		count++
+	} else {
+		count += 2
+	}
+
+	mid := count / 2
+
+	return ll[mid]
+}
---
+++ 876-middle-of-the-linked-list/solution_test.go
@@ -0,0 +1,105 @@
+package middle_of_the_linked_list
+
+import (
+	"testing"
+
+	"github.com/nemca/leetcode/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMiddleNode(t *testing.T) {
+	type args struct {
+		head *models.ListNode
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want *models.ListNode
+	}{
+		{
+			name: "example 1",
+			args: args{
+				head: &models.ListNode{
+					Val: 1,
+					Next: &models.ListNode{
+						Val: 2,
+						Next: &models.ListNode{
+							Val: 3,
+							Next: &models.ListNode{
+								Val: 4,
+								Next: &models.ListNode{
+									Val:  5,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &models.ListNode{
+				Val: 3,
+				Next: &models.ListNode{
+					Val: 4,
+					Next: &models.ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+		{
+			name: "example 2",
+			args: args{
+				head: &models.ListNode{
+					Val: 1,
+					Next: &models.ListNode{
+						Val: 2,
+						Next: &models.ListNode{
+							Val: 3,
+							Next: &models.ListNode{
+								Val: 4,
+								Next: &models.ListNode{
+									Val: 5,
+									Next: &models.ListNode{
+										Val:  6,
+										Next: nil,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &models.ListNode{
+				Val: 4,
+				Next: &models.ListNode{
+					Val: 5,
+					Next: &models.ListNode{
+						Val:  6,
+						Next: nil,
+					},
+				},
+			},
+		},
+		{
+			name: "example 3",
+			args: args{
+				head: &models.ListNode{
+					Val:  1,
+					Next: nil,
+				},
+			},
+			want: &models.ListNode{
+				Val:  1,
+				Next: nil,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, middleNode(tt.args.head))
+		})
+	}
+}
