d3ffbb31e15c5bee54c4d05e5b0d4eb0cd2d6b93
Michael Bruskov <mixanemca@yandex.ru>
2025-07-24 20:16:46 +0400

    add fmt.Stringer interface implementation for models.ListNode

--- models/linked_list.go
+++ models/linked_list.go
@@ -1,6 +1,25 @@
 package models
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+// String implements fmt.Stringer interface.
+func (l *ListNode) String() string {
+	var sb strings.Builder
+	sb.WriteString("[")
+	for node := l; node != nil; node = node.Next {
+		sb.WriteString(fmt.Sprintf("%d", node.Val))
+		if node.Next != nil {
+			sb.WriteString(" -> ")
+		}
+	}
+	sb.WriteString("]")
+	return sb.String()
 }
